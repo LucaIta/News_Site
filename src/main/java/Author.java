9d729100236dc3bb1a29efbac5b5d82f1e41f07c
Luca Quatela <lucaqq@gmail.com>
2016-08-19 16:42:04 +0200

    add member variables to Author object

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,3 +1,19 @@
 public class Author {
-  
+  private String name;
+  private String role;
+  private String bio;
+  private String picture;
+  private String email;
+  private String facebook;
+  private String twitter;
+
+  public Author(String name,String role,String bio,String picture,String email,String facebook,String twitter) {
+    this.name = name;
+    this.role = role;
+    this.bio = bio;
+    this.picture= picture;
+    this.email = email;
+    this.facebook = facebook;
+    this.twitter = twitter;
+  }
 }
--- src/test/java/AuthorTest.java
+++ src/test/java/AuthorTest.java
@@ -5,7 +5,7 @@ public class AuthorTest {
 
   @Test
   public void author_instantiatesCorrectly_true() {
-    Author newAuthor = new Author();
+    Author newAuthor = new Author("Luca M","Reporter", "Born in may", "www.testUrl.com", "luca@gmail.com", "facebookLink", "twitterLink");
     assertTrue(newAuthor instanceof Author);
   }
 }
