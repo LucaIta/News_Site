2d4de9e9092d43c2716666cbdbb63d44692ae627
Luca Quatela <lucaqq@gmail.com>
2016-08-21 12:12:09 +0200

    add new author form and path and save new authors correctly

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,6 +23,27 @@ public class App {
       String authorByLine = request.queryParams("authorByLine");
       Article newArticle = new Article(title,shortTitle,body,picture,subhead,subtitle,authorByLine);
       newArticle.save();
+      response.redirect("/articles/new");
+      return null;
+    });
+
+    get("/authors/new", (request,response) -> {
+      HashMap<String,Object> model = new HashMap<String,Object>();
+      model.put("template", "/templates/author-form.vtl");
+      return new ModelAndView(model, "/templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    post("/authors", (request,response) -> {
+      String name = request.queryParams("name");
+      String role = request.queryParams("role");
+      String bio = request.queryParams("bio");
+      String picture = request.queryParams("picture");
+      String email = request.queryParams("email");
+      String facebook = request.queryParams("facebook");
+      String twitter = request.queryParams("twitter");
+      Author newAuthor = new Author(name,role,bio,picture,email,facebook,twitter);
+      newAuthor.save();
+      response.redirect("/authors/new");
       return null;
     });
 
